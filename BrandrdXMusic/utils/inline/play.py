73dec1d5514a888dbfebfb396fbe9a1be1815bc0
KajalXMusic <ansh68798@gmail.com>
2024-06-11 01:26:35 +0530

    Update play.py

--- BrandrdXMusic/utils/inline/play.py
+++ BrandrdXMusic/utils/inline/play.py
@@ -71,7 +71,7 @@ def stream_markup_timer(_, vidid, chat_id, played, dur):
                 text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/ANSHKING82"
             ),
             InlineKeyboardButton(
-                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/ANSH_KAA_WORLD"
+                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/anshsharmagroup"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
